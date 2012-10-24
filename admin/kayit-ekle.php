645e59ed918607260ed58c206420cf630395fa05
Şan Güneş <antarktikali@gmail.com>
2012-10-24 04:27:24 +0300

    Sandbox dosyasından kopi peystin azizliği

--- admin/kayit-ekle.php
+++ admin/kayit-ekle.php
@@ -25,7 +25,7 @@ if($_POST){
   if(empty($name)){
     $errors["name"]       = "Lütfen Adı, Soyadı giriniz.";
   }
-  if(empty($cellnumber) || !preg_match("/^((\+90)|0)?[1-9][0-9]{9}$/", $num)){
+  if(empty($cellnumber) || !preg_match("/^((\+90)|0)?[1-9][0-9]{9}$/", $cellnumber){
     $errors["cellnumber"] = "Lütfen Telefon numarası giriniz.";
   }
   if(empty($email)){
